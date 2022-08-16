db694b5c41ab63f357a562aa18910ca27fe004d5
Maaloum <50062179+maaloum@users.noreply.github.com>
2022-08-17 01:14:50 +0300

    webpack exercice

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html',
+    }),
+  ],
+  optimization: {
+
+    runtimeChunk: 'single',
+
+  },
+};
